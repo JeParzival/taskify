86b73a950e096d4aa4393d0bb0bd8f144a14b823
aloshai <arma.55@outlook.com>
2022-04-02 07:28:01 +0300

    routers defined to app

--- taskify-frontend/src/App.tsx
+++ taskify-frontend/src/App.tsx
@@ -1,24 +1,35 @@
-import { useState, useEffect } from "react";
-import { Navigate } from "react-router-dom";
+// views
+import ProfileView from './views/ProfileView';
+import TasksView from './views/TasksView';
+import TeamsView from './views/TeamsView';
+import TeamView from './views/TeamView';
+import CreateTeamView from './views/CreateTeamView';
+import RegisterView from "./views/RegisterView";
+import LoginView from "./views/LoginView";
+
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+} from "react-router-dom";
+
+import AuthProvider from './components/auth/AuthProvider';
+import RequireAuth from './components/auth/RequireAuth';
+
 function App() {
-  useEffect(() => {
-    if(window && typeof window != "undefined") {
-      setWindow(window);
-      if(!window.localStorage.getItem("mail") || !window.localStorage.getItem("password")) {
-         window.location.href = "/register"
-         return
-      }
-      
-    }
-    
-  })
-  const [user, setUser] = useState<any>();
-  const [windower, setWindow] = useState<any>();
-  return (
-    <div className="App">
-      <h1>test</h1>
-    </div>
-  );
+  return <AuthProvider>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/user" element={<RequireAuth><ProfileView /></RequireAuth>} />
+        <Route path="/tasks" element={<RequireAuth><TasksView /></RequireAuth>} />
+        <Route path="/teams" element={<RequireAuth><TeamsView /></RequireAuth>} />
+        <Route path="/team" element={<RequireAuth><TeamView /></RequireAuth>} />
+        <Route path="/createTeam" element={<RequireAuth><CreateTeamView /></RequireAuth>} />
+        <Route path="/register" element={<RegisterView />} />
+        <Route path="/login" element={<LoginView />} />
+      </Routes>
+    </BrowserRouter>
+  </AuthProvider>
 }
 
 export default App;
--- taskify-frontend/src/index.tsx
+++ taskify-frontend/src/index.tsx
@@ -2,31 +2,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 
-import ProfileView from './views/ProfileView';
-import TasksView from './views/TasksView';
-import TeamsView from './views/TeamsView';
-import TeamView from './views/TeamView';
-import CreateTeamView from './views/CreateTeamView';
-import Register from "./views/Register";
-import Login from "./views/Login";
-import {
-  BrowserRouter,
-  Routes,
-  Route
-} from "react-router-dom";
-
 ReactDOM.render(
-  <BrowserRouter>
-    <Routes>
-      <Route path="/" element={<App />} />
-      <Route path="/user" element={<ProfileView />} />
-      <Route path="/tasks" element={<TasksView />} />
-      <Route path="/teams" element={<TeamsView />} />
-      <Route path="/team" element={<TeamView />} />
-      <Route path="/createTeam" element={<CreateTeamView />} />
-      <Route path="/register" element={<Register />} />
-      <Route path="/login" element={<Login />} />
-    </Routes>
-  </BrowserRouter>,
+  <App />,
   document.getElementById('root')
 );
